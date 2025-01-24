f77c367e40586c7d86eff0554361861d8fc39906
Aramais <ar4maissarkisyan@yandex.ru>
2025-01-24 20:05:08 +0300

    Этап3_Задание1_Саркисян_Арамаис

---
+++ create_note_function.py
@@ -0,0 +1,83 @@
+from datetime import datetime, timedelta
+
+
+# Создание функции для создания заметок
+def create_note():
+    created_date = datetime.now().date().strftime("%d-%m-%Y")
+
+    # Запрашиваем имя пользователя
+    while True:
+        username = input("Введите имя пользователя: ")
+        while not username:
+            print("Поле не может быть пустым!!!")
+            username = input("Введите имя пользователя: ")
+        break
+
+    # Запрашиваем заголовок
+    while True:
+        title = input("Введите заголовок заметки: ")
+        while not title:
+            print("Поле не может быть пустым: ")
+            title = input("Введите заголовок заметки: ")
+        break
+
+    # Запрашиваем описание заметки
+    while True:
+        content = input("Введите описание заметки: ")
+        while not content:
+            print("Описание не может быть пустым!")
+            content = input("Введите описание заметки: ")
+        break
+
+    # Запрашиваем статус
+    while True:
+        status = input("Введите статус заметки (новая, в процессе, выполнена): ").strip().lower()
+        while status not in ("новая", "в процессе", "выполнена"):
+            print("Ответ неверный повторите ввод!")
+            status = input("Введите статус заметки (новая, в процессе, выполнена): ").strip().lower()
+        break
+
+    # Дата создания заметки устанавливает автоматически текущую дату
+    deadline = datetime.strptime(created_date, "%d-%m-%Y").date()
+
+    # Запрашиваем дату дедлайна если дата не введена устанавливается по умолчанию 1 неделя
+    while True:
+        issue_date = input("Введите дату дедлайна в формате (день-месяц-год): ")
+        while not issue_date:
+            issue_date = deadline + timedelta(days=7)
+            issue_date = issue_date.strftime("%d-%m-%Y")
+            print("Установлена дата дедлайна по умолчанию.")
+            break
+        try:
+            datetime.strptime(issue_date, "%d-%m-%Y")
+
+        except ValueError:
+            print("Ошибка вы ввели неверные данные!!!\n"
+                  "Пример ввода (05-01-2025)")
+            continue
+        break
+
+    # Добавляем полученные данные в словарь
+    note = {
+        'username': username,
+        'title': title,
+        'content': content,
+        'status': status,
+        'created_date': created_date,
+        'issue_date': issue_date,
+    }
+
+    print("\nЗаметка успешно создана.")
+
+    # Получаем готовый словарь
+    return note
+
+
+if __name__ == '__main__':
+    note = create_note()
+    print(f"\nИмя пользователя: {note['username']}")
+    print(f"Заголовок: {note['title']}")
+    print(f"Описание заметки: {note['content']}")
+    print(f"Статус заметки: {note['status']}")
+    print(f"Дата создания заметки: {note['created_date']}")
+    print(f"Дата дедлайна: {note['issue_date']}")
